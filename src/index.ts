4a68db4e39bea54e8b7ba4f25a93c51769ce028b
Duknam Yoo <yoo2001818@gmail.com>
2018-12-14 23:48:12 +0900

    Change to use airbnb lint rules

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 type Action = {
   type: 'set' | 'increment' | 'decrement' | 'remove',
   key: string[],
-  value: any
+  value: any,
 };
 
-export function getActionScopes (action: Action): ([string[], string])[] {
+export function getActionScopes(action: Action): ([string[], string])[] {
   if (['increment', 'decrement'].includes(action.type)) {
     return [[action.key, 'add']];
   }
   return [[action.key, null]];
 }
 
-export async function merge (
-  offendingScope: string[][], local: Action[], remote: Action[]
+export async function merge(
+  offendingScope: string[][], local: Action[], remote: Action[],
 ) {
   return {
     local: [] as Action[],
-    remote: [] as Action[]
+    remote: [] as Action[],
   };
 }
