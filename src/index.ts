5a930e8fd81d35eebb1249d81a320b0e722849cd
Duknam Yoo <yoo2001818@gmail.com>
2019-02-23 15:16:43 +0900

    Implement JSON machine

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,81 @@
 import Machine from './machine';
 import MemoryStorage from './storage/memory';
-import { Action } from './type';
+import { Action, ActionScope } from './type';
 
 type ActionData = {
   type: 'set' | 'delete' | 'increment' | 'decrement',
-  key: string[],
+  keys: string[],
   value?: any,
 };
 
-type ActionUndoData = {};
+type ActionUndoData = { prevValue?: any };
 
 type JSONAction = Action<ActionData, ActionUndoData>;
 
+const dataStore: any = {};
+
+function findNode(keys: string[], root: any) {
+  return keys.reduce((prev, key, i) => {
+    if (i === keys.length - 1) return prev;
+    return prev[key];
+  }, root);
+}
+
 const machine = new Machine({
-  getScopes: (action: JSONAction) => {
+  getScopes: (data: ActionData): ActionScope[] => {
+    return [{
+      keys: data.keys,
+      modifyType: data.type === 'increment' || data.type === 'decrement' ?
+        0 :
+        null,
+    }];
+  },
+  getReverse: (data: ActionData, undoData: ActionUndoData): ActionData => {
+    switch (data.type) {
+      case 'set':
+      case 'delete':
+        return { type: 'set', keys: data.keys, value: undoData.prevValue };
+      case 'increment':
+        return { type: 'decrement', keys: data.keys };
+      case 'decrement':
+        return { type: 'increment', keys: data.keys };
+    }
+  },
+  merge: async (
+    offending: (string | number)[][],
+    left: JSONAction[],
+    right: JSONAction[],
+  ) => {
+    throw new Error(offending.map(v => v.join(', ')).join('_'));
+  },
+  run: async (data: ActionData): Promise<ActionUndoData> => {
+    const key = data.keys[data.keys.length - 1];
+    const node = findNode(data.keys, dataStore);
+    switch (data.type) {
+      case 'set': {
+        const prevValue = node[key];
+        node[key] = data.value;
+        return { prevValue };
+      }
+      case 'delete': {
+        const prevValue = node[key];
+        delete node[key];
+        return { prevValue };
+      }
+      case 'increment': {
+        node[key] += 1;
+        return {};
+      }
+      case 'decrement': {
+        node[key] -= 1;
+        return {};
+      }
+    }
   },
 }, new MemoryStorage());
+
+async function main() {
+  await machine.run({ type: 'set', keys: ['user', 'name'], value: 'test' });
+}
+
+main();
